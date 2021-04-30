b042a534b3e2ef6270e176f782e859a65ef55ccc
Amaan <amaanaliakhtar@gmail.com>
2021-04-30 12:17:00 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// jshint esversion:6
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+//Add starting code
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = {
+	email: String,
+	password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+//RENDERING PAGES
+app.get("/", function (req, res) {
+	res.render("home");
+});
+
+app.get("/login", function (req, res) {
+	res.render("login");
+});
+
+app.get("/register", function (req, res) {
+	res.render("register");
+});
+
+app.post("/register", function (req, res) {
+	//creating new user from inputs from register page
+	const newUser = new User({
+		email: req.body.username,
+		password: req.body.password,
+	});
+
+	newUser.save(function (err) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("secrets");
+		}
+	});
+});
+
+//LEVEL 1 Authentication - check usernames and passwords against DB values
+app.post("/login", function (req, res) {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	User.findOne({ email: username }, function (err, foundUser) {
+		if (err) {
+			console.log(err);
+		} else {
+			if (foundUser) {
+				if (foundUser.password === password) {
+					res.render("secrets");
+				}
+			}
+		}
+	});
+});
+
+//SEVER START
+app.listen(3000, function () {
+	console.log("Server started in port 3000");
+});
